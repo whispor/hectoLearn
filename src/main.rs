68eea27e724a08f9a295fcd0e564357f6c834793
Tako <whispor@outlook.com>
2023-12-16 14:31:06 +0800

    使用crossterm监测键盘输入

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-<<<<<<< HEAD
-use std::io::{self, stdout, Read};
-use crossterm::terminal::enable_raw_mode;
+use std::{io::{self, stdout}, char};
+use crossterm::{terminal::{enable_raw_mode},
+    event::{Event, KeyCode, KeyModifiers, read, self}};
 
 fn to_ctrl_byte(c:char) -> u8{
     let byte = c as u8;
@@ -13,28 +13,51 @@ fn die(e: std::io::Error){
 
 fn main() {
     println!("Hello, world!");
-    enable_raw_mode();
+    let _ = enable_raw_mode();
 
     let _stdout = stdout();
 
-    for b in io::stdin().bytes(){
-        match b {
-            Ok(b) =>{
-                let c = b as char;
-                if c.is_control(){
-                    println!("{:?} \r",b);
-                }else{
-                    println!("{:?} ({}) \r", b, c);
-                }
-                if b== to_ctrl_byte('q'){
-                    break;
+    // for b in io::stdin().bytes(){
+    //     match b {
+    //         Ok(b) =>{
+    //             let c = b as char;
+    //             if c.is_control(){
+    //                 println!("{:?} \r",b);
+    //             }else{
+    //                 println!("{:?} ({}) \r", b, c);
+    //             }
+    //             if b== to_ctrl_byte('q'){
+    //                 break;
+    //             }
+    //         },
+    //         Err(err) => die(err),
+    //     }
+    // }
+
+
+
+
+    while let Ok(pressed_key) = read(){
+        match pressed_key {
+            Event::Key(event)  => {
+                match (event.modifiers, event.code) {
+                    (KeyModifiers::CONTROL, KeyCode::Char('q')) | (_, KeyCode::Esc) => {
+                        break;
+                    },
+
+                    (_, KeyCode::Char(c)) => {
+                        if c.is_control(){
+                            println!("{:?} \r", c as u8);
+                        }else{
+                            println!("{:?} ({}) \r", c as u8, c);
+                        }
+                    },
+
+                    (_,_) => println!("{:?} \r", pressed_key),
                 }
             },
-            Err(err) => die(err),
+            _ => println!("{}","hahah")
         }
     }
-=======
-fn main() {
-    println!("Hello, world!");
->>>>>>> be198f70ee6accfe441e209743d9464c6247577e
+
 }
