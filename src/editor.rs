5d822d31a2a9361a7cf12a2d0cde1ff032a44e57
Tako <whispor@outlook.com>
2023-12-16 15:24:05 +0800

    将run中功能分离到不同函数

--- src/editor.rs
+++ src/editor.rs
@@ -13,34 +13,66 @@ fn die(e: std::io::Error){
 
 
 pub struct Editor{
-
+    should_quit: bool,
 }
 
 impl Editor{
+    pub fn default() -> Self{
+        Self{should_quit:false}
+    }
+
     pub fn run(&self){
         let _ = enable_raw_mode();
         let _stdout = stdout();
 
-        while let Ok(pressed_key) = read(){
-            match pressed_key {
-                Event::Key(event)  => {
-                    match (event.modifiers, event.code) {
-                        (KeyModifiers::CONTROL, KeyCode::Char('q')) | (_, KeyCode::Esc) => {
-                            break;
-                        },
+        // while let Ok(pressed_key) = read(){
+        //     match pressed_key {
+        //         Event::Key(event)  => {
+        //             match (event.modifiers, event.code) {
+        //                 (KeyModifiers::CONTROL, KeyCode::Char('q')) | (_, KeyCode::Esc) => {
+        //                     break;
+        //                 },
     
-                        (_, KeyCode::Char(c)) => {
-                            if c.is_control(){
-                                println!("{:?} \r", c as u8);
-                            }else{
-                                println!("{:?} ({}) \r", c as u8, c);
-                            }
-                        },
+        //                 (_, KeyCode::Char(c)) => {
+        //                     if c.is_control(){
+        //                         println!("{:?} \r", c as u8);
+        //                     }else{
+        //                         println!("{:?} ({}) \r", c as u8, c);
+        //                     }
+        //                 },
     
-                        (_,_) => println!("{:?} \r", pressed_key),
-                    }
-                },
-                _ => println!("{}","hahah")
+        //                 (_,_) => println!("{:?} \r", pressed_key),
+        //             }
+        //         },
+        //         _ => println!("{}","hahah")
+        //     }
+        // }
+
+        loop {
+            if let Err(error) = self.process_keypress(){
+                die(error);
+            }
+        }
+    }
+
+
+    fn process_keypress(&self) -> Result<(), std::io::Error>{
+        let pressed_key = Self::read_key()?;
+        match pressed_key {
+            Event::Key(key) => match (key.modifiers, key.code){
+                (KeyModifiers::CONTROL, KeyCode::Char('q')) => panic!("Program End."),
+                (_, _) => Ok(())
+            },
+            _ => Ok(())
+        }
+
+    }
+
+    fn read_key() -> Result<Event, std::io::Error>{
+        loop {
+            let event = read();
+            if let Ok(Event::Key(_)) = event{
+                return event
             }
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,6 @@ use editor::Editor;
 fn main() {
     println!("Hello, world!");
 
-    let editor = Editor{};
+    let editor = Editor::default();
     editor.run();
 }
