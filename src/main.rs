28dd846a1be60f95add1cf24f43f7cf760e15fde
北夜 <36132293+whispor@users.noreply.github.com>
2023-12-16 10:44:35 +0800

    修改输出格式

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use std::io::{self, stdout, Read};
+use crossterm::terminal::enable_raw_mode;
+
+fn to_ctrl_byte(c:char) -> u8{
+    let byte = c as u8;
+    byte & 0b0001_1111
+}
+
+fn die(e: std::io::Error){
+    panic!("{}",e);
+}
+
+fn main() {
+    println!("Hello, world!");
+    enable_raw_mode();
+
+    let _stdout = stdout();
+
+    for b in io::stdin().bytes(){
+        match b {
+            Ok(b) =>{
+                let c = b as char;
+                if c.is_control(){
+                    println!("{:?} \r",b);
+                }else{
+                    println!("{:?} ({}) \r", b, c);
+                }
+                if b== to_ctrl_byte('q'){
+                    break;
+                }
+            },
+            Err(err) => die(err),
+        }
+    }
+}
---
+++ target/debug/deps/hecto-50acda636c5a56dd.d
@@ -0,0 +1,5 @@
+E:\Coding\hecto\target\debug\deps\libhecto-50acda636c5a56dd.rmeta: src\main.rs
+
+E:\Coding\hecto\target\debug\deps\hecto-50acda636c5a56dd.d: src\main.rs
+
+src\main.rs:
